d6a4a82b6e31cd9fe3c707d100bb9e81f2678b1f
daniil <daniil.romanchenko@gmail.com>
2022-03-31 12:26:25 +0200

    add remove /build folder

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniil/IdeaProjects/mk-tfhe-auction/src
-# Build directory: /home/daniil/IdeaProjects/mk-tfhe-auction/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("libtfhe")
-subdirs("test")
--- build/test/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/daniil/IdeaProjects/mk-tfhe-auction/src/test/googletest/googlemock
-# Build directory: /home/daniil/IdeaProjects/mk-tfhe-auction/build/test/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
