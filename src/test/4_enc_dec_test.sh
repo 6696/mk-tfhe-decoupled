5440fe90665f9d144d3c3192190ee3c6a92a8147
daniil <daniil.romanchenko@gmail.com>
2022-04-01 22:32:42 +0200

    add some e2e tests

---
+++ src/test/4_enc_dec_test.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# generate keys
+for i in 1 2 3 4
+do
+	cd client$i
+	../mk_tfhe_client-spqlios-fma g $i
+	cd ..
+	echo -e '\n'
+done
+
+# create encrypted string for client 1
+cd client1
+../mk_tfhe_client-spqlios-fma e 101001
+cd ..
+
+# encrypt bit sequence with other clients' keys
+for i in 2 3 4
+do
+        cd client$i
+        ../mk_tfhe_client-spqlios-fma n ../client1/sampleSeq1.binary
+        cd ..
+	echo -e '\n'
+done
+
+#################################################
+
+# decrypt bit sequence with other clients' keys
+for i in 1 2 3 4
+do
+        cd client$i
+        ../mk_tfhe_client-spqlios-fma d ../client1/sampleSeq1.binary
+        cd ..
+	echo -e '\n'
+done
+
+# finalize bit sequence and show result
+./mk_tfhe_client-spqlios-fma f client1/sampleSeq1.binary
+
---
+++ src/test/reset.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# generate keys
+for i in 1 2 3 4
+do
+	cd client$i
+	rm sampleSeq$i.binary 
+	rm keys/KSKBSK.binary
+	rm keys/Public.binary
+	rm keys/Secret.binary
+	cd ..
+	echo -e '\n'
+done
+
